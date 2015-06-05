3883d1632c5f0b1a24998329121c3dd9c03888a3
Liu Jing <jing.liu@rea-group.com>
2015-06-05 17:42:13 +0800

    import butterknife

--- app/src/main/java/com/myapp/twer/myapplication/MyActivity.java
+++ app/src/main/java/com/myapp/twer/myapplication/MyActivity.java
@@ -1,21 +1,54 @@
 package com.myapp.twer.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.InjectView;
 
 
 public class MyActivity extends Activity {
+
+    @InjectView(R.id.button)
+    Button button;
+
+    @InjectView(R.id.hello_world)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                start2();
+                textView.setText("Hello");
+            }
+        });
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("TAG", "onStart");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("TAG", "onPause");
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,7 +69,10 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void start2(View view) {
+    public void start2() {
         Log.e("TAG", "start 2");
+        Intent intent = new Intent();
+        intent.setClass(this, MyActivity2.class);
+        startActivity(intent);
     }
 }
