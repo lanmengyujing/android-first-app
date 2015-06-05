5868da94fb806435d9b281de70bb0c6ca4826916
Liu Jing <jing.liu@rea-group.com>
2015-06-05 17:07:09 +0800

    add second activity

--- app/src/main/java/com/myapp/twer/myapplication/MyActivity2.java
+++ app/src/main/java/com/myapp/twer/myapplication/MyActivity2.java
@@ -2,25 +2,23 @@ package com.myapp.twer.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import com.myapp.twer.myapplication.R;
 
-
-public class MyActivity extends Activity {
+public class MyActivity2 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_my_activity2);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.my_activity2, menu);
         return true;
     }
 
@@ -34,9 +32,5 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    public void start2(View view) {
-        Log.e("TAG", "start 2");
     }
 }
